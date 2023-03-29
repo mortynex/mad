a12ffb5cb8cbcd469775fa01998f6e382a171245
mortynex <mortynex@outlook.com>
2023-03-29 20:19:52 +0200

    feat: REPL mode

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,39 @@
-import { tokenize } from "./lexer.ts";
+import { tokenize, TokenTypes } from "./lexer.ts";
+const startREPL = (processor: (input: string) => any) => {
+	while (true) {
+		const code = prompt("> ");
 
-console.log(tokenize("1").next());
+		if (!code) {
+			continue;
+		}
+
+		if (code?.startsWith(";")) {
+			switch (code.slice(1).toLowerCase()) {
+				case "cls":
+					console.log("\n".repeat(100));
+					break;
+			}
+
+			continue;
+		}
+
+		let result: any = null;
+
+		try {
+			result = processor(code);
+		} catch (e) {
+			console.error(e);
+
+			continue;
+		}
+
+		console.log(result);
+	}
+};
+
+startREPL((code: string) => {
+	return tokenize(code)
+		.array()
+		.map(({ type }) => TokenTypes[type])
+		.join(" _ ");
+});
